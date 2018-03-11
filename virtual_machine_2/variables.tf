0c7e03b85945bca48a42c5725dfb5302757e7403
Tom Straub <tstraub@cardinalsolutions.com>
2018-03-11 16:33:54 -0500

    More modules, same repo

---
+++ virtual_machine_2/variables.tf
@@ -0,0 +1,55 @@
+variable "resource_group_name" {
+  type = "string"
+  description = "The name of the existing Resource Group to deploy the VMs to"
+}
+variable "name" {}
+variable "location" {}
+variable "subnet_id" {}
+variable "username" {}
+variable "password" {}
+variable "os" {
+  type        = "string"
+  description = "The Marketplace image information in the following format: Offer:Publisher:Sku:Version"
+}
+
+variable "count" {
+  default     = 2
+  type        = "string"
+  description = "The number of VMs to deploy"
+}
+
+variable "size" {
+  default     = "Standard_D1_v2"
+  type        = "string"
+  description = ""
+}
+
+variable "disk_os_sku" {
+  default     = "Standard_LRS"
+  type        = "string"
+  description = ""
+}
+
+variable "disk_data_sku" {
+  default     = "Standard_LRS"
+  type        = "string"
+  description = ""
+}
+
+variable "disk_data_size_gb" {
+  default     = 128
+  type        = "string"
+  description = ""
+}
+
+variable "delete_os_disk_on_termination" {
+  default     = false
+  type        = "string"
+  description = ""
+}
+
+variable "delete_data_disks_on_termination" {
+  default     = false
+  type        = "string"
+  description = ""
+}
